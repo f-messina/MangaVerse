63a6a6a869bdbd81fc65e241b0d22ad3ec0dd888
noemi <noemicherchi@gmail.com>
2024-01-19 19:18:12 +0100

    Updated MangaDAO and Manga

--- src/main/java/it/unipi/lsmsd/fnf/dao/base/BaseNeo4JDAO.java
+++ src/main/java/it/unipi/lsmsd/fnf/dao/base/BaseNeo4JDAO.java
@@ -1,4 +1,28 @@
 package it.unipi.lsmsd.fnf.dao.base;
 
+import org.neo4j.driver.AuthTokens;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+
 public abstract class BaseNeo4JDAO {
+    private static final String NE04J_URI = "bolt://localhost:7687";
+    private static final String NEO4J_USERNAME = "neo4j";
+    private static final String NEO4J_PASSWORD = "neo4j/neo4j";
+
+    private static final Driver driver = GraphDatabase.driver(
+            NE04J_URI,
+            AuthTokens.basic(NEO4J_USERNAME, NEO4J_PASSWORD)
+    );
+
+    public static Driver getDriver() {
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if(driver != null) {
+            driver.close();
+        }
+    }
+
+
 }
