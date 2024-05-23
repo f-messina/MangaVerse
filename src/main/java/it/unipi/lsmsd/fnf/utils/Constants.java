c28b591d0946f22983ef841451b1e8e666ee3ce2
Flavio Messina <messinaflavio00@gmail.com>
2024-05-23 21:29:44 +0200

    css profile page and resolved bugs

--- src/main/java/it/unipi/lsmsd/fnf/utils/Constants.java
+++ src/main/java/it/unipi/lsmsd/fnf/utils/Constants.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 public class Constants {
 
     public static final String DEFAULT_PROFILE_PICTURE = "images/account-icon.png";
+    public static final String DEFAULT_COVER_MANGA = "images/manga-image-default.png";
     public static final String AUTHENTICATED_USER_KEY = "authenticatedUser";
     public static final int PAGE_SIZE = 25;
     public static final String PAGINATION_FACET = "paginationResults";
